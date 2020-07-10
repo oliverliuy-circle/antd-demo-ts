35f45ef43ad6708cf71ecfbe24f879622164a8ff
oliverliuy <oliverliuy@qq.com>
2020-07-10 17:59:38 +0800

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Button, Divider, Radio } from 'antd'
+import './App.less';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      </header> */}
+      <Divider>
+        <Button type="primary">Button</Button>
+        <br></br>
+        <Radio>是</Radio>
+        <Radio>不是</Radio>
+      </Divider>
     </div>
   );
 }
-
 export default App;
