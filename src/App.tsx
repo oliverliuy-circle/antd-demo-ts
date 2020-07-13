08cf7c8f8d6c8a59625a6c5111a917eadef3bafa
oliverliuy <oliverliuy@qq.com>
2020-07-13 18:13:27 +0800

    pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React, { FC } from 'react';
 import logo from './logo.svg';
-import { Button, Divider, Radio } from 'antd'
 import './App.less';
+import { Breadcrumb, Menu } from 'antd'
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-      </header> */}
-      <Divider>
-        <Button type="primary">Button</Button>
-        <br></br>
-        <Radio>是</Radio>
-        <Radio>不是</Radio>
-      </Divider>
+      </header>
+      <div className="ant-layout-top">
+        <div className="ant-layout-header">
+          <div className="ant-layout-wrapper">
+            <div className="ant-layout-logo"></div>
+            <Menu theme="dark" mode="horizontal"
+              defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
+              <Menu.Item key="1">导航一</Menu.Item>
+              <Menu.Item key="2">导航二</Menu.Item>
+              <Menu.Item key="3">导航三</Menu.Item>
+            </Menu>
+          </div>
+        </div>
+        <div className="ant-layout-subheader">
+          <div className="ant-layout-wrapper">
+            <Menu mode="horizontal"
+              defaultSelectedKeys={['1']} style={{ marginLeft: 124 }}>
+              <Menu.Item key="1">二级导航</Menu.Item>
+              <Menu.Item key="2">二级导航</Menu.Item>
+              <Menu.Item key="3">二级导航</Menu.Item>
+            </Menu>
+          </div>
+        </div>
+        <div className="ant-layout-wrapper">
+          <div className="ant-layout-breadcrumb">
+            <Breadcrumb>
+              <Breadcrumb.Item>首页</Breadcrumb.Item>
+              <Breadcrumb.Item>应用列表</Breadcrumb.Item>
+              <Breadcrumb.Item>某应用</Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <div className="ant-layout-container">
+            <div style={{ height: 400 }}></div>
+          </div>
+        </div>
+        <div className="ant-layout-footer">Ant Design 版权所有 © 2015 由蚂蚁金服体验技术部支持</div>
+      </div>
     </div>
   );
 }
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+import React, { JSXElementConstructor } from 'react'
+import ReactDOM from 'react-dom'
+import './App.less'
+
+function Clock(props: any): JSX.Element {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>现在是 {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+function tick(): void {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('example')
+  );
+}
+
+setInterval(tick, 1000);
