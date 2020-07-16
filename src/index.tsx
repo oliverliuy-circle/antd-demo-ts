a8c01f1cca2bcc4a12e5499a18d21437245b0334
oliverliuy <oliverliuy@qq.com>
2020-07-16 18:51:01 +0800

    pull

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,39 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
-import React, { JSXElementConstructor } from 'react'
+import React, { Component, MouseEvent, MouseEventHandler, Requireable } from 'react'
 import ReactDOM from 'react-dom'
-import './App.less'
+import PropTypes from 'prop-types'
+import Item from 'antd/lib/list/Item'
 
-function Clock(props: any): JSX.Element {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>现在是 {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+interface BlogProps {
+  posts: {
+    id: number
+    title: string
+    content: string
+  }[]
 }
-
-function tick(): void {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('example')
-  );
+class Blog extends Component<BlogProps>{
+  lis: JSX.Element[] = this.props.posts.map(
+    (item) => <li key={item.id}>{item.title}</li>
+  )
+  hs: JSX.Element[] = this.props.posts.map(
+    (item) => <div> <h1 key={'h1' + item.id}>{item.title}</h1><h2 key={'h2' + item.id}>{item.content}</h2></div>
+  )
+  render(): JSX.Element {
+    return (
+      <div>
+        <ul>{this.lis}</ul>
+        <hr></hr>
+        {this.hs}
+      </div>
+    )
+  }
 }
-
-setInterval(tick, 1000);
+const posts: {
+  id: number
+  title: string
+  content: string
+}[] = [
+    { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+    { id: 2, title: 'Installation', content: 'You can install React from npm.' },
+    { id: 3, title: 'oliver', content: 'hello oliver.' }
+  ];
+ReactDOM.render(<Blog posts={posts} />, document.getElementById('example'))
